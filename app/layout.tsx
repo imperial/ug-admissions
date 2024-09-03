fc4ffc2c91cbcfc8ba25eff16dd243f3d19523ba
Matthew Alex <matalex412@gmail.com>
2024-09-03 15:04:43 +0100

    style: add padding to page content
    Co-authored-by: IlliaDerevianko <IlliaDerevianko@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + ' p-6'}>
         <Theme>{children}</Theme>
       </body>
     </html>
