9724c81fe7ae193ec35d75b5c56710d97315b138
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-09-04 11:24:44 +0100

    refactor: use esm import instead cjs require

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
+import classNames from 'classnames'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import 'tailwindcss/tailwind.css'
 
 const inter = Inter({ subsets: ['latin'] })
-const classNames = require('classnames')
 
 export const metadata: Metadata = {
   title: 'Create Next App',
