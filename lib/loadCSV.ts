a20a875ae76ff3507e31c13414553d3dffcc3d73
Matthew Alex <matalex412@gmail.com>
2024-09-06 17:20:16 +0100

    feat: user inputting with applications

--- lib/loadCSV.ts
+++ lib/loadCSV.ts
@@ -1,20 +1,54 @@
 'use server'
 
 import prisma from '@/db'
+import { Applicant } from '@prisma/client'
 import { parse } from 'csv-parse/sync'
 import { redirect } from 'next/navigation'
+
+// uploadUsers
+// uploadTmuaScores/extra application data
+// uploadOutcomes
 
 export const uploadApplicants = async (formData: FormData) => {
   const csv = formData.get('csv') as File
   const lines = await csv.text()
 
-  const applicants = parse(lines, {
+  const applicants: Applicant[] = parse(lines, {
     columns: true,
     skip_empty_lines: true
   })
-  console.log(applicants)
-  await prisma.applicant.createMany({
-    data: applicants
-  })
+
+  // in both cases, create a new application record with initial 'nextAction' as 'TMUA_CANDIDATE' if no TMUA score
+
+  const applicantUpserts = applicants.map((a) =>
+    prisma.applicant.upsert({
+      where: {
+        cid: a.cid
+      },
+      update: {
+        cid: a.cid,
+        ucasNumber: a.ucasNumber,
+        gender: a.gender,
+        firstName: a.firstName,
+        surname: a.surname,
+        email: a.email,
+        primaryNationality: a.primaryNationality,
+        otherNationality: a.otherNationality
+      },
+      create: {
+        cid: a.cid,
+        ucasNumber: a.ucasNumber,
+        gender: a.gender,
+        firstName: a.firstName,
+        surname: a.surname,
+        email: a.email,
+        primaryNationality: a.primaryNationality,
+        otherNationality: a.otherNationality
+      }
+    })
+  )
+
+  await Promise.all(applicantUpserts)
+
   redirect('/')
 }
