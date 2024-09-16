026c54ef1559fccf18c9b1ab2e3164216593906b
Zaki <zaki.amin20@imperial.ac.uk>
2024-09-16 16:09:57 +0100

    chore: update seed so an internal review is created for all applications

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -67,8 +67,6 @@ const createReview = (application: Application) => {
     motivationAdminScore: faker.number.float({ multipleOf: 0.1, min: 0.0, max: 10.0 }),
     extracurricularAdminScore: faker.number.float({ multipleOf: 0.1, min: 0.0, max: 10.0 }),
     examComments: faker.person.bio(),
-    lastAdminEditBy:
-      faker.string.alpha({ length: 2 }).toLowerCase() + faker.string.numeric({ length: 3 }),
     application: {
       connect: {
         id: application.id
@@ -83,9 +81,7 @@ async function main() {
 
     for (let j = 0; j < 10; j++) {
       const application = await prisma.application.create({ data: createApplication(user) })
-      if (j % 2 == 0) {
-        await prisma.internalReview.create({ data: createReview(application) })
-      }
+      await prisma.internalReview.create({ data: createReview(application) })
     }
   }
 }
