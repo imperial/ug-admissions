6a443b5fe1c999ca2cb5bcfabc1e5d1f984c5c71
Illia Derevianko <iliaderevianko30@gmail.com>
2024-09-18 17:43:28 +0200

    style: fix table alignment and button padding

--- components/UgTutorDialog.tsx
+++ components/UgTutorDialog.tsx
@@ -200,7 +200,7 @@ const UgTutorDialog: FC<UgTutorDialogProps> = ({ data }) => {
       title="UG Tutor Form"
       isOpen={isOpen}
       onOpenChange={setIsOpen}
-      trigger={<Button>UG Tutor Form</Button>}
+      trigger={<Button className="min-h-12">UG Tutor Form</Button>}
     >
       <FormWrapper
         action={currentTab === 'outcomes' ? upsertOutcomeWithId : addCommentWithId}
