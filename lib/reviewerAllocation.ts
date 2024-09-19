3a99aae55fdbf283512f3162051a722682b23d6c
Illia Derevianko <iliaderevianko30@gmail.com>
2024-09-19 16:23:59 +0200

    refactor: extract partitioning in a separate function + fix small error

--- lib/reviewerAllocation.ts
+++ lib/reviewerAllocation.ts
@@ -4,7 +4,7 @@ import { Application, Role } from '@prisma/client'
 /**
  * Retrieves reviewers with application count from the database
  */
-const getReviewersWithApplicationCount = async () =>
+const getReviewersWithAscApplicationCount = async () =>
   await prisma.user.findMany({
     where: {
       role: Role.REVIEWER
@@ -15,16 +15,22 @@ const getReviewersWithApplicationCount = async () =>
           applications: true
         }
       }
+    },
+    orderBy: {
+      applications: {
+        _count: 'asc'
+      }
     }
   })
 
 /**
- * Accepts a list of applications to a specific reviewer
+ * Accepts a list of applications and assigns to a specific reviewer
  * @param applications - a list of applications that will be assigned a reviewer
  * @param reviewerId - a reviewer id to be assigned to the applications
+ * @returns - a Promise that resolves to the updated user object
  */
-const allocateApplicationsToReviewer = async (applications: Application[], reviewerId: number) => {
-  await prisma.user.update({
+const allocateApplicationsToReviewer = (applications: Application[], reviewerId: number) =>
+  prisma.user.update({
     where: {
       id: reviewerId
     },
@@ -34,6 +40,30 @@ const allocateApplicationsToReviewer = async (applications: Application[], revie
       }
     }
   })
+
+/**
+ * Divides a total number into equal partitions and distributes the remainder evenly from the beginning.
+ * @param total - The total number of items to be divided into partitions.
+ * @param parts - The number of partitions to create.
+ * @returns An array of tuples, where each tuple contains the start and end
+ *          indices of each partition, represented as [startIndex, endIndex].
+ *          The indices can be used to slice an array into the desired partitions.
+ * @example
+ * // Example: Dividing 10 items into 3 partitions
+ * const partitions = getEqualPartition(10, 3);
+ * // partitions will be: [[0, 4], [4, 7], [7, 10]]
+ */
+const getEqualPartition = (total: number, parts: number): [number, number][] => {
+  const quotient = Math.floor(total / parts)
+  const remainder = total % parts
+  const result: [number, number][] = []
+  let added = 0
+  for (let i = 0; i < parts; i++) {
+    const intervalLen = quotient + (i < remainder ? 1 : 0)
+    result.push([added, added + intervalLen])
+    added += intervalLen
+  }
+  return result
 }
 
 /**
@@ -43,35 +73,23 @@ const allocateApplicationsToReviewer = async (applications: Application[], revie
  *
  *  The function:
  *  - Retrieves the current application count per reviewer.
- *  - Computes the fair share of applications for each reviewer based on the total applications (new and already assigned).
- *  - Assigns applications accordingly, taking into account any remainder to ensure all applications are allocated.
+ *  - Gets a partition to split the total applications evenly across the reviewers (including the remainder).
+ *  - Assigns applications accordingly in an asynchronous manner.
  * @param applications
  */
 export const allocateApplications = async (applications: Application[]) => {
   // assign reviewers only to applications that don't have one
   applications = applications.filter((application) => application.reviewerId === null)
-  const applicationsCount = applications.length
-  const reviewersWithCount = await getReviewersWithApplicationCount()
+  const reviewersWithCount = await getReviewersWithAscApplicationCount()
   const reviewersCount = reviewersWithCount.length
   if (reviewersCount === 0)
     throw new Error(
       'Reviewer allocation failed because no reviewers were found. Please upload reviewers and try again.'
     )
-  const assignedApplicationsCount = reviewersWithCount.reduce(
-    (acc, reviewer) => acc + reviewer._count.applications,
-    0
+
+  const slices = getEqualPartition(applications.length, reviewersCount)
+  const allocations = reviewersWithCount.map((reviewer, i) =>
+    allocateApplicationsToReviewer(applications.slice(...slices[i]), reviewer.id)
   )
-  const totalApplicationsCount = applicationsCount + assignedApplicationsCount
-  const quotient = Math.floor(totalApplicationsCount / reviewersCount)
-  const remainder = totalApplicationsCount % reviewersCount
-  let assignedCount = 0
-  for (const reviewer of reviewersWithCount) {
-    const i = reviewersWithCount.indexOf(reviewer)
-    const newApplicationsCount = quotient - reviewer._count.applications + Number(i < remainder)
-    await allocateApplicationsToReviewer(
-      applications.slice(assignedCount, assignedCount + newApplicationsCount),
-      reviewer.id
-    )
-    assignedCount += newApplicationsCount
-  }
+  await Promise.all(allocations)
 }
