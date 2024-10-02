11762716b9cf61cab40cb118494233211669e2bc
Zaki <zaki.amin20@imperial.ac.uk>
2024-10-02 17:17:50 +0100

    feat: bar graph of #applications in each stage using recharts

--- app/statistics/[cycle]/page.tsx
+++ app/statistics/[cycle]/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from '@/auth'
 import AdmissionsCycleStatistics from '@/components/AdmissionsCycleStatistics'
 import prisma from '@/db'
+import { Decision, NextAction } from '@prisma/client'
 import { redirect } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
@@ -19,11 +20,50 @@ export default async function AdmissionsCycleStatisticsPage({
   const userEmail = session?.user?.email as string
   const cycle = parseInt(params.cycle)
 
-  const noApplications = await prisma.application.count({
+  // count applications because applicants don't have cycles
+  // ignore different outcomes per degree code
+  const noApplicants = await prisma.application.count({
     where: {
       admissionsCycle: cycle
     }
   })
 
-  return <AdmissionsCycleStatistics cycle={Number(params.cycle)} noApplications={noApplications} />
+  const outcomes = await prisma.outcome.findMany({
+    where: {
+      application: {
+        admissionsCycle: cycle
+      }
+    }
+  })
+  const noOffers = outcomes.filter((outcome) => outcome.decision === Decision.OFFER).length
+  const noRejections = outcomes.filter((outcome) => outcome.decision === Decision.REJECT).length
+
+  // Count the number of applications with each type of nextAction
+  const nextActionCounts: Record<NextAction, number> = {
+    ADMIN_SCORING: 0,
+    CANDIDATE_INFORMED: 0,
+    INFORM_CANDIDATE: 0,
+    REVIEWER_SCORING: 0,
+    TMUA_CANDIDATE: 0,
+    TMUA_RESULT: 0,
+    UG_TUTOR_REVIEW: 0
+  }
+  for (const nextAction of Object.values(NextAction)) {
+    nextActionCounts[nextAction] = await prisma.application.count({
+      where: {
+        admissionsCycle: cycle,
+        nextAction
+      }
+    })
+  }
+
+  return (
+    <AdmissionsCycleStatistics
+      cycle={Number(params.cycle)}
+      noApplicants={noApplicants}
+      noOffers={noOffers}
+      noRejections={noRejections}
+      nextActionCounts={nextActionCounts}
+    />
+  )
 }
--- components/AdmissionsCycleStatistics.tsx
+++ components/AdmissionsCycleStatistics.tsx
@@ -1,15 +1,61 @@
+'use client'
+
+import { NextAction } from '@prisma/client'
 import { DataList, Flex, Heading } from '@radix-ui/themes'
 import React, { FC } from 'react'
+import {
+  Bar,
+  BarChart,
+  Legend,
+  Rectangle,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts'
 
 interface AdmissionsCycleStatisticsProps {
   cycle: number
-  noApplications: number
+  noApplicants: number
+  noOffers: number
+  noRejections: number
+  nextActionCounts: Record<NextAction, number>
+}
+
+interface NextActionGraphProps {
+  data: { name: string; quantity: number }[]
+}
+
+const NextActionGraph: FC<NextActionGraphProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="quantity"
+          fill="#8884d8"
+          activeBar={<Rectangle fill="pink" stroke="blue" />}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  )
 }
 
 const AdmissionsCycleStatistics: FC<AdmissionsCycleStatisticsProps> = ({
   cycle,
-  noApplications
+  noApplicants,
+  noOffers,
+  noRejections,
+  nextActionCounts
 }) => {
+  const nextActionGraphData = Object.entries(nextActionCounts).map(([nextAction, count]) => ({
+    name: nextAction,
+    quantity: count
+  }))
+
   return (
     <Flex direction="column" gap="4">
       <Heading>Undergraduate Admissions Portal</Heading>
@@ -19,14 +65,24 @@ const AdmissionsCycleStatistics: FC<AdmissionsCycleStatisticsProps> = ({
       <Heading as="h2" size="3">
         Summary statistics
       </Heading>
+
       <DataList.Root>
         <DataList.Item>
-          <DataList.Label>Total number of applications:</DataList.Label>
-          <DataList.Value>{noApplications}</DataList.Value>
+          <DataList.Label>Total number of applicants:</DataList.Label>
+          <DataList.Value>{noApplicants}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label color="green">Offers made:</DataList.Label>
+          <DataList.Value>{noOffers}</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label color="red">Rejections:</DataList.Label>
+          <DataList.Value>{noRejections}</DataList.Value>
         </DataList.Item>
       </DataList.Root>
+
+      <NextActionGraph data={nextActionGraphData} />
     </Flex>
   )
 }
-
 export default AdmissionsCycleStatistics
