498d9d009e0c5caa6c68c28d6a91f03d63cdf6c4
Zaki <zaki.amin20@imperial.ac.uk>
2024-10-09 12:21:20 +0100

    refactor: tidy up ord for NextAction

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,22 +14,9 @@ export function decimalToString(value: Decimal | null, defaultString: string = '
   return converted.toString()
 }
 
+const orderedActions: NextAction[] = Object.values(NextAction)
+
 // Define a numerical order on Prisma enum, ordinarily done by string comparison
 export function ord(nextAction: NextAction): number {
-  switch (nextAction) {
-    case 'ADMIN_SCORING_MISSING_TMUA':
-      return 0
-    case 'ADMIN_SCORING_WITH_TMUA':
-      return 1
-    case 'PENDING_TMUA':
-      return 2
-    case 'REVIEWER_SCORING':
-      return 3
-    case 'UG_TUTOR_REVIEW':
-      return 4
-    case 'INFORM_CANDIDATE':
-      return 5
-    case 'CANDIDATE_INFORMED':
-      return 6
-  }
+  return orderedActions.indexOf(nextAction)
 }
