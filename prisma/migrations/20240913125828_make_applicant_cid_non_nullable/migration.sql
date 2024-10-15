865bb9b60201f90793fffc1c02eba2113574820f
Zaki <zaki.amin20@imperial.ac.uk>
2024-10-15 10:52:30 +0100

    refactor: rename 'applicantCid' to 'cid' in application table for consistency

--- prisma/migrations/20240913125828_make_applicant_cid_non_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `applicantCid` on table `Application` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Application" ALTER COLUMN "applicantCid" SET NOT NULL;
