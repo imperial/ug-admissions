4dc188eac7d20127b6014b399c073da529abecea
Zaki Amin <88145585+zaki-amin@users.noreply.github.com>
2024-10-24 14:52:47 +0100

    chore: drop otherNationality from applicant  (#30)
    chore: drop otherNationality from applicant because this data is not provided

---
+++ prisma/migrations/20241024112924_remove_other_nationality_from_applicant/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `otherNationality` on the `Applicant` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Applicant" DROP COLUMN "otherNationality";
