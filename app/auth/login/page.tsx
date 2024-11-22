03f77c6562201966633ba1b093f8ae17c41e842b
Zaki <zaki.amin20@imperial.ac.uk>
2024-11-22 14:24:01 +0000

    fix: downgrade NextAuth and change signIn form and library

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,24 +1,40 @@
-'use client'
-
+import { signIn } from '@/auth'
 import { EnterIcon } from '@radix-ui/react-icons'
 import { Button, Flex, Heading, Separator, Text } from '@radix-ui/themes'
-import { signIn } from 'next-auth/react'
+import { AuthError } from 'next-auth'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const LoginPage = async () => {
+const LoginPage = async ({ searchParams }: { searchParams?: { callbackUrl?: string } }) => {
+  const signInEntraID = async () => {
+    'use server'
+    try {
+      await signIn('microsoft-entra-id', {
+        redirectTo: searchParams?.callbackUrl ?? '/'
+      })
+    } catch (error) {
+      if (error instanceof AuthError) {
+        return redirect('/auth/error')
+      }
+      throw error
+    }
+  }
+
   return (
     <Flex gap="6" direction="column">
       <Flex pl="9" pr="9" direction="column" gap="5">
-        <Flex direction="column" justify="center" align="center">
+        <Flex direction="column" justify="center" align="center" gap="1">
           <Heading as="h1">Undergraduate Admissions</Heading>
-          <Text>Portal for managing UG admissions</Text>
+          <Text>Portal for managing UG admissions at DoC, Imperial College London</Text>
         </Flex>
         <Separator size="4" />
         <Flex direction="column" gap="3">
-          <Button onClick={() => signIn('microsoft-entra-id', { redirectTo: '/' })}>
-            <EnterIcon />
-            Sign In
-          </Button>
+          <form action={signInEntraID}>
+            <Button size="3" className="w-full">
+              Login with SSO
+              <EnterIcon />
+            </Button>
+          </form>
         </Flex>
       </Flex>
     </Flex>
--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import authConfig from '@/auth.config'
 import NextAuth from 'next-auth'
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   cookies: {
     pkceCodeVerifier: {
