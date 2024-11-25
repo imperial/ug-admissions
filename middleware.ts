2bb6b1eeaf31fef643fcb6c1d06c585f3bb45674
Zaki <zaki.amin20@imperial.ac.uk>
2024-11-25 16:10:30 +0000

    refactor: redo middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,5 @@
-import NextAuth from 'next-auth'
-
-import authConfig from './auth.config'
+export { auth as middleware } from '@/auth'
 
 export const config = {
-  /*
-   * Match all request paths except for the ones starting with:
-   * - api (API routes)
-   * - _next/static (static files)
-   * - _next/image (image optimization files)
-   * - favicon.ico (favicon file)
-   * - images (static images from public directory)
-   * - login (login page)
-   * Also do not match home path
-   */
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|login|auth|$).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
-
-export const { auth: middleware } = NextAuth(authConfig)
