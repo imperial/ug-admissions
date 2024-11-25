0d339a8f000f4989ab9239570dc569d80e3c2703
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-11-25 13:38:56 +0000

    fix: remove explicit cookie configuration

--- auth.ts
+++ auth.ts
@@ -3,16 +3,5 @@ import NextAuth from 'next-auth'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  cookies: {
-    pkceCodeVerifier: {
-      name: 'authjs.pkce.code_verifier',
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true
-      }
-    }
-  },
   session: { strategy: 'jwt' }
 })
